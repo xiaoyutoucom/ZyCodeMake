f643d0348b5fc4b0af670da785edfc296e06a6d0
煙花綻放過後只剩黑暗 <576165539@qq.com>
2021-07-15 14:52:45 +0800

    no commit message

---
+++ ZyCodeMake/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ZyCodeMake
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDbConnect = new System.Windows.Forms.TextBox();
+            this.clb_Tables = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCodeMakeAll = new System.Windows.Forms.Button();
+            this.btnOpenPath = new System.Windows.Forms.Button();
+            this.txtNameSpace = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkAllTables = new System.Windows.Forms.CheckBox();
+            this.chkModel = new System.Windows.Forms.CheckBox();
+            this.chkRepository = new System.Windows.Forms.CheckBox();
+            this.chkInterface = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(811, 25);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "读取";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "连接字符串：";
+            // 
+            // txtDbConnect
+            // 
+            this.txtDbConnect.Location = new System.Drawing.Point(97, 27);
+            this.txtDbConnect.Name = "txtDbConnect";
+            this.txtDbConnect.Size = new System.Drawing.Size(695, 21);
+            this.txtDbConnect.TabIndex = 2;
+            this.txtDbConnect.Text = "Server=192.168.1.244;Port=5432;Database=CityLawEnforce;User Id=postgres;Password=" +
+    "admin;";
+            // 
+            // clb_Tables
+            // 
+            this.clb_Tables.CheckOnClick = true;
+            this.clb_Tables.FormattingEnabled = true;
+            this.clb_Tables.Location = new System.Drawing.Point(16, 143);
+            this.clb_Tables.Name = "clb_Tables";
+            this.clb_Tables.Size = new System.Drawing.Size(872, 276);
+            this.clb_Tables.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "数据库表：";
+            // 
+            // btnCodeMakeAll
+            // 
+            this.btnCodeMakeAll.ForeColor = System.Drawing.Color.Crimson;
+            this.btnCodeMakeAll.Location = new System.Drawing.Point(482, 443);
+            this.btnCodeMakeAll.Name = "btnCodeMakeAll";
+            this.btnCodeMakeAll.Size = new System.Drawing.Size(236, 23);
+            this.btnCodeMakeAll.TabIndex = 5;
+            this.btnCodeMakeAll.Text = "【生成选择表的数据仓储代码】";
+            this.btnCodeMakeAll.UseVisualStyleBackColor = true;
+            this.btnCodeMakeAll.Click += new System.EventHandler(this.btnCodeMakeAll_Click);
+            // 
+            // btnOpenPath
+            // 
+            this.btnOpenPath.Location = new System.Drawing.Point(742, 443);
+            this.btnOpenPath.Name = "btnOpenPath";
+            this.btnOpenPath.Size = new System.Drawing.Size(144, 23);
+            this.btnOpenPath.TabIndex = 6;
+            this.btnOpenPath.Text = "打开代码所在文件夹";
+            this.btnOpenPath.UseVisualStyleBackColor = true;
+            this.btnOpenPath.Click += new System.EventHandler(this.btnOpenPath_Click);
+            // 
+            // txtNameSpace
+            // 
+            this.txtNameSpace.Location = new System.Drawing.Point(97, 68);
+            this.txtNameSpace.Name = "txtNameSpace";
+            this.txtNameSpace.Size = new System.Drawing.Size(439, 21);
+            this.txtNameSpace.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "命名空间：";
+            // 
+            // chkAllTables
+            // 
+            this.chkAllTables.AutoSize = true;
+            this.chkAllTables.Location = new System.Drawing.Point(96, 111);
+            this.chkAllTables.Name = "chkAllTables";
+            this.chkAllTables.Size = new System.Drawing.Size(48, 16);
+            this.chkAllTables.TabIndex = 9;
+            this.chkAllTables.Text = "全选";
+            this.chkAllTables.UseVisualStyleBackColor = true;
+            this.chkAllTables.CheckedChanged += new System.EventHandler(this.chkAllTables_CheckedChanged);
+            // 
+            // chkModel
+            // 
+            this.chkModel.AutoSize = true;
+            this.chkModel.Checked = true;
+            this.chkModel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkModel.Location = new System.Drawing.Point(85, 443);
+            this.chkModel.Name = "chkModel";
+            this.chkModel.Size = new System.Drawing.Size(54, 16);
+            this.chkModel.TabIndex = 10;
+            this.chkModel.Text = "Model";
+            this.chkModel.UseVisualStyleBackColor = true;
+            // 
+            // chkRepository
+            // 
+            this.chkRepository.AutoSize = true;
+            this.chkRepository.Checked = true;
+            this.chkRepository.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRepository.Location = new System.Drawing.Point(158, 443);
+            this.chkRepository.Name = "chkRepository";
+            this.chkRepository.Size = new System.Drawing.Size(84, 16);
+            this.chkRepository.TabIndex = 11;
+            this.chkRepository.Text = "Repository";
+            this.chkRepository.UseVisualStyleBackColor = true;
+            // 
+            // chkInterface
+            // 
+            this.chkInterface.AutoSize = true;
+            this.chkInterface.Checked = true;
+            this.chkInterface.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkInterface.Location = new System.Drawing.Point(248, 443);
+            this.chkInterface.Name = "chkInterface";
+            this.chkInterface.Size = new System.Drawing.Size(78, 16);
+            this.chkInterface.TabIndex = 12;
+            this.chkInterface.Text = "InterFace";
+            this.chkInterface.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 443);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "生成文件：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(552, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "【必填】";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 479);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkInterface);
+            this.Controls.Add(this.chkRepository);
+            this.Controls.Add(this.chkModel);
+            this.Controls.Add(this.chkAllTables);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNameSpace);
+            this.Controls.Add(this.btnOpenPath);
+            this.Controls.Add(this.btnCodeMakeAll);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clb_Tables);
+            this.Controls.Add(this.txtDbConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRead);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.Text = "PostgreSQL数据库C#代码生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDbConnect;
+        private System.Windows.Forms.CheckedListBox clb_Tables;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCodeMakeAll;
+        private System.Windows.Forms.Button btnOpenPath;
+        private System.Windows.Forms.TextBox txtNameSpace;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkAllTables;
+        private System.Windows.Forms.CheckBox chkModel;
+        private System.Windows.Forms.CheckBox chkRepository;
+        private System.Windows.Forms.CheckBox chkInterface;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ZyCodeMake/Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace ZyCodeMake
+{
+    public partial class FormMain : Form
+    {
+        string filePath = System.IO.Directory.GetCurrentDirectory() + "\\OutFile";
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;//设置边框为不可调节
+            this.MaximizeBox = false;//取消最大化按键
+            this.MinimizeBox = false;//取消最小化按键
+        }
+
+        //读取连接字符串
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string str_DbConnect = this.txtDbConnect.Text;
+            if (string.IsNullOrEmpty(str_DbConnect))
+            {
+                MessageBox.Show("请填写连接字符串！");
+                return;
+            }
+
+            //先清空
+            this.clb_Tables.Items.Clear();
+
+            NpgsqlConnection conn;
+            try
+            {
+                conn = new NpgsqlConnection(str_DbConnect);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("连接失败！ex：" + ex.Message.ToString());
+                return;
+            }
+            using (conn)
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("连接失败！ex：" + ex.Message.ToString());
+                    return;
+                }
+                
+
+                string sql_tables = @"
+select relname 表名,cast(obj_description(relfilenode,'pg_class') as varchar) 名称 
+  from pg_class
+ where relname in (select tablename from pg_tables where schemaname='public' and position('_2' in tablename)=0)
+ order by relname asc
+";
+
+
+                using (var cmd = new NpgsqlCommand(sql_tables, conn))
+                using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                    this.clb_Tables.Items.Add(reader["表名"].ToString() + "  |  " + reader["名称"].ToString());
+            }
+
+            //全选判断
+            chkAllTablesFun();
+        }
+
+        //生成代码
+        private void btnCodeMakeAll_Click(object sender, EventArgs e)
+        {
+            //判断
+            if (this.clb_Tables.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("请选择表！");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.txtNameSpace.Text.ToString().Trim()))
+            {
+                MessageBox.Show("请填写命名空间！");
+                return;
+            }
+
+            //连接字符串
+            string str_DbConnect = this.txtDbConnect.Text;
+            //命名空间
+            string strNameSpace = this.txtNameSpace.Text.ToString().Trim();
+            //选择的表
+            string[] arrTables = new string[this.clb_Tables.CheckedItems.Count];
+            this.clb_Tables.CheckedItems.CopyTo(arrTables, 0);
+            
+            //sql语句
+            string sql_select = @"
+SELECT col_description(a.attrelid,a.attnum) as ColComment,
+				format_type(a.atttypid,a.atttypmod) as ColType,
+				a.attname as ColName, 
+				a.attnotnull as ColNotNull   
+	FROM pg_class as c,pg_attribute as a 
+ WHERE c.relname = '{0}' 
+   and a.attrelid = c.oid 
+   and a.attnum>0
+   and a.attisdropped = false
+";
+            string sql_PK_select = @"
+select pg_attribute.attname as colname,pg_type.typname as typename,pg_constraint.conname as pk_name from 
+pg_constraint  inner join pg_class 
+on pg_constraint.conrelid = pg_class.oid 
+inner join pg_attribute on pg_attribute.attrelid = pg_class.oid 
+and  pg_attribute.attnum = pg_constraint.conkey[1]
+inner join pg_type on pg_type.oid = pg_attribute.atttypid
+where pg_class.relname = '{0}' 
+and pg_constraint.contype='p'
+";
+
+            //最终表信息
+            List<TableInfo> list = new List<TableInfo>();
+
+            for (int i = 0; i < arrTables.Length; i++)
+            {
+                TableInfo model = new TableInfo()
+                {
+                    TableName = arrTables[i].Split('|')[0].Trim(),
+                    TableComment = arrTables[i].Split('|')[1].Trim(),
+                    List_TableFieldInfo = new List<TableFieldInfo>()
+                };
+                string sql = string.Format(sql_select, model.TableName);
+                string sql_PK = string.Format(sql_PK_select, model.TableName);
+
+
+                using (var conn = new NpgsqlConnection(str_DbConnect))
+                {
+                    conn.Open();
+
+                    //读出所有字段
+                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    using (var dr = cmd.ExecuteReader())
+                    while (dr.Read())
+                    {
+                        TableFieldInfo fieldModel = new TableFieldInfo();
+                        fieldModel.ColComment = ObjToString(dr["ColComment"]);
+                        fieldModel.ColType = ObjToString(dr["ColType"]);
+                        fieldModel.ColName = ObjToString(dr["ColName"]);
+                        fieldModel.ColNotNull = ObjToString(dr["ColNotNull"]);
+
+                        model.List_TableFieldInfo.Add(fieldModel);
+                    }
+                    //找到主键
+                    using (var cmd = new NpgsqlCommand(sql_PK, conn))
+                    using (var dr = cmd.ExecuteReader())
+                    while (dr.Read())
+                    {
+                        model.TablePK = ObjToString(dr["colname"]);
+                    }
+                }
+                list.Add(model);
+            }
+
+            //代码生成方法
+            CodeMakeFun(list, strNameSpace);
+
+            MessageBox.Show("生成成功！");
+        }
+
+        //代码生成方法
+        private void CodeMakeFun(List<TableInfo> list, string strNameSpace)
+        {
+            foreach (TableInfo item in list)
+            {
+                //1.生成Model
+                if (this.chkModel.Checked)
+                    Model_Make(item, strNameSpace);
+
+
+                //2.生成Repository
+                if (this.chkRepository.Checked)
+                    Repository_Make(item, strNameSpace);
+
+                //3.生成Interface
+                if (this.chkInterface.Checked)
+                    InterFace_Make(item, strNameSpace);
+            }
+        }
+
+        private void Model_Make(TableInfo model, string strNameSpace)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format(@"/******************************************
+* 模块名称：Model-{0}
+* 当前版本：1.0
+* 生成时间：{1}
+* 版本历史：此代码由正元地理信息代码生成工具自动生成。
+* 
+******************************************/
+using System;
+using Robin.Domain.Entities;
+using Robin.NHibernate.EntityMappings;", 
+ToUppperFirst(model.TableName), DateTime.Now.ToString("yyyy/MM/dd")));
+
+            sb.AppendLine("namespace " + strNameSpace);
+            sb.AppendLine("{");
+            sb.AppendLine("\tpublic class "+ ToUppperFirst(model.TableName) + "Map : EntityMap<"+ ToUppperFirst(model.TableName) + ", string>");
+            sb.AppendLine("\t{");
+            sb.AppendLine("\t\tpublic " + ToUppperFirst(model.TableName) + "Map() : base(\""+ model.TableName + "\")");
+            sb.AppendLine("\t\t{");
+            foreach (TableFieldInfo item in model.List_TableFieldInfo)
+            {
+                //主键
+                if (item.ColName.ToLower() == ObjToString(model.TablePK).ToLower())
+                {
+                    sb.AppendLine("\t\t\tId(x => x."+ item.ColName.ToUpper() + ");");
+                }
+                else
+                {
+                    sb.AppendLine("\t\t\tMap(x => x." + item.ColName.ToUpper() + ");");
+                }
+            }
+            
+            sb.AppendLine("\t\t}");
+            sb.AppendLine("\t}");
+            sb.AppendLine("\tpublic class "+ ToUppperFirst(model.TableName) + " : Entity<string>");
+            sb.AppendLine("\t{");
+            sb.AppendLine("\t\t#region 公共属性");
+            foreach (TableFieldInfo item in model.List_TableFieldInfo)
+            {
+                //主键
+                if (item.ColName.ToLower() == ObjToString(model.TablePK).ToLower())
+                {
+                    continue;
+                }
+
+                sb.AppendLine("\t\t/// <summary>");
+                sb.AppendLine("\t\t/// " + item.ColComment.Replace("\r\n", "") + "");
+                sb.AppendLine("\t\t/// </summary>");
+                if (item.ColType.ToLower().Contains("character"))
+                {
+                    sb.AppendLine("\t\tpublic virtual string " + item.ColName.ToUpper() + " { get; set; }");
+                }
+                else if (item.ColType.ToLower().Contains("numeric"))
+                {
+                    sb.AppendLine("\t\tpublic virtual Nullable<decimal> " + item.ColName.ToUpper() + " { get; set; }");
+                }
+                else
+                {
+                    sb.AppendLine("\t\tpublic virtual string " + item.ColName.ToUpper() + " { get; set; }");
+                }
+            }
+            sb.AppendLine("\t\t#endregion");
+            sb.AppendLine("\t}");
+            sb.AppendLine("}");
+           
+            FileCreate(ToUppperFirst(model.TableName) + ".cs", sb.ToString());
+        }
+
+        private void Repository_Make(TableInfo model, string strNameSpace)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format(@"/******************************************
+* 模块名称：DataAccess-{0}
+* 当前版本：1.0
+* 生成时间：{1}
+* 版本历史：此代码由正元地理信息代码生成工具自动生成。
+* 
+******************************************/
+using NHibernate;
+using Robin.NHibernate;
+using Robin.NHibernate.Repositories;",
+ToUppperFirst(model.TableName)+ "Repository", DateTime.Now.ToString("yyyy/MM/dd")));
+
+            sb.AppendLine("namespace " + strNameSpace);
+            sb.AppendLine("{");
+            sb.AppendLine("\t/// <summary>");
+            sb.AppendLine("\t/// " + (string.IsNullOrEmpty(model.TableComment) ? model.TableName : model.TableComment) + "仓储");
+            sb.AppendLine("\t/// </summary>");
+            sb.AppendLine("\tpublic class " + ToUppperFirst(model.TableName) + "Repository : NhRepositoryBase<" + ToUppperFirst(model.TableName) + ", string>, I" + ToUppperFirst(model.TableName) + "Repository");
+            sb.AppendLine("\t{");
+            sb.AppendLine("\t\tpublic "+ ToUppperFirst(model.TableName) + "Repository(ISessionProvider sessionProvider): base(sessionProvider)");
+            sb.AppendLine("\t\t{");
+            sb.AppendLine("\t\t}");
+            sb.AppendLine("\t}");
+            sb.AppendLine("}");
+
+            FileCreate(ToUppperFirst(model.TableName) + "Repository.cs", sb.ToString());
+        }
+
+        private void InterFace_Make(TableInfo model, string strNameSpace)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format(@"/******************************************
+* 模块名称：Core-{0}
+* 当前版本：1.0
+* 生成时间：{1}
+* 版本历史：此代码由正元地理信息代码生成工具自动生成。
+* 
+******************************************/
+using Robin.Domain.Repositories;",
+"I" + ToUppperFirst(model.TableName) + "Repository", DateTime.Now.ToString("yyyy/MM/dd")));
+
+            sb.AppendLine("namespace " + strNameSpace);
+            sb.AppendLine("{");
+            sb.AppendLine("\t/// <summary>");
+            sb.AppendLine("\t/// " + (string.IsNullOrEmpty(model.TableComment) ? model.TableName : model.TableComment) + "仓储");
+            sb.AppendLine("\t/// </summary>");
+
+            sb.AppendLine("\tpublic interface I"+ ToUppperFirst(model.TableName) + "Repository : IRepository<"+ ToUppperFirst(model.TableName) + ", string>");
+            sb.AppendLine("\t{");
+            sb.AppendLine("\t}");
+            sb.AppendLine("}");
+
+            FileCreate("I" + ToUppperFirst(model.TableName) + "Repository.cs", sb.ToString());
+        }
+
+        //首字母大写
+        private string ToUppperFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length < 2)
+                return s;
+            
+            return s.Substring(0, 1).ToUpper() + s.Substring(1);
+        }
+
+        //对象转string
+        private string ObjToString(object obj)
+        {
+            if (obj == null)
+                return "";
+            else
+                return obj.ToString();
+        }
+
+        //创建文件
+        private void FileCreate(string FileName, string strContent)
+        {
+            if (System.IO.Directory.Exists(filePath) == false)
+            {
+                System.IO.Directory.CreateDirectory(filePath);
+            }
+
+            File.WriteAllText(filePath + "\\" + FileName, strContent);
+        }
+
+        //打开文件夹
+        private void btnOpenPath_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Explorer.exe", filePath);
+        }
+
+        //设置全选操作
+        private void chkAllTables_CheckedChanged(object sender, EventArgs e)
+        {
+            chkAllTablesFun();
+        }
+        //设置全选方法
+        private void chkAllTablesFun()
+        {
+            bool checkFlag = false;
+            if (this.chkAllTables.Checked)
+                checkFlag = true;
+            else
+                checkFlag = false;
+
+            for (int j = 0; j < this.clb_Tables.Items.Count; j++)
+                this.clb_Tables.SetItemChecked(j, checkFlag);
+        }
+    }
+
+    public class TableInfo
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 表名备注
+        /// </summary>
+        public string TableComment { get; set; }
+
+        /// <summary>
+        /// 表主键
+        /// </summary>
+        public string TablePK { get; set; }
+
+        /// <summary>
+        /// 每列
+        /// </summary>
+        public List<TableFieldInfo> List_TableFieldInfo { get; set; }
+    }
+
+
+    public class TableFieldInfo
+    {
+        /// <summary>
+        /// 字段备注
+        /// </summary>
+        public string ColComment { get; set; }
+
+        /// <summary>
+        /// 字段类型
+        /// </summary>
+        public string ColType { get; set; }
+
+        /// <summary>
+        /// 字段名称
+        /// </summary>
+        public string ColName { get; set; }
+
+        /// <summary>
+        /// 字段是否为null
+        /// </summary>
+        public string ColNotNull { get; set; }
+    }
+}
